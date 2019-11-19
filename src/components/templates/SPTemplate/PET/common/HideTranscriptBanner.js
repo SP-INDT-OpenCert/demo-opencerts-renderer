a92e39d2fe573be8d183c7b5bd3a537238bf36e1
Ryan Sena <ryan.o.sena@gmail.com>
2019-11-19 14:21:59 +0800

    modified:   src/components/templates/SPTemplate/PET/common/HideTranscriptBanner.js 	deleted:    src/components/templates/SPTemplate/PET/common/SimplePrivacyFilterBanner.js

--- src/components/templates/SPTemplate/PET/common/HideTranscriptBanner.js
+++ src/components/templates/SPTemplate/PET/common/HideTranscriptBanner.js
@@ -5,27 +5,16 @@ import { faCut } from "@fortawesome/free-solid-svg-icons";
 
 const HideTranscriptBanner = ({ hideTranscript }) => (
   <div id="banner-privacy-filter" className="screen-only">
-    <div
-      className="row"
-      style={{
-        backgroundColor: "whitesmoke",
-        padding: 20,
-        marginBottom: 20
-      }}
+    <div className="row"
+      style={{backgroundColor: "whitesmoke", padding: 20, marginBottom: 20 }}
     >
-      <div style={{ display: "inline-block" }}>
-        <div className="h4">OpenCerts Transcript hiding Enabled</div>
-        <div>
-          Remove this Transcript by clicking on the cut button. 
-		  Downloaded certificate remains valid!
-        </div>
-      </div>
-      <div
-        className="ml-auto h3 pointer"
-        style={{ display: "inline-block" }}
-        onClick={hideTranscript}
-      >
+      <div className="h4" style={{ display: "inline-block" }}>OpenCerts Transcript hiding Enabled</div>
+      <div className="ml-auto h3 pointer" style={{ display: "inline-block" }} onClick={hideTranscript} >
         <FontAwesomeIcon icon={faCut} cursor="pointer" />
+      </div>
+      <div>
+          click on the "Scissors ICON" to remove your transcript if you wish to email your "opencert" file,
+		  without your transcript, to the recipient
       </div>
     </div>
   </div>
