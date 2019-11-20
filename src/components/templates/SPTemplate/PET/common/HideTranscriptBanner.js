3bdbc30f1b308c92f91d3ba2893390b29043e44d
Ryan Sena <ryan.o.sena@gmail.com>
2019-11-20 16:41:26 +0800

    modified:   src/components/templates/SPTemplate/PET/common/HideTranscriptBanner.js

--- src/components/templates/SPTemplate/PET/common/HideTranscriptBanner.js
+++ src/components/templates/SPTemplate/PET/common/HideTranscriptBanner.js
@@ -8,17 +8,17 @@ import { faFileDownload } from "@fortawesome/free-solid-svg-icons";
 const HideTranscriptBanner = ({ hideTranscript }) => (
   <div id="banner-privacy-filter" className="screen-only">
     <div className="row"
-      style={{backgroundColor: "whitesmoke", padding: 20, marginBottom: 20 }}
+      style={{backgroundColor: "whitesmoke", color: "steelblue", padding: 20, marginBottom: 20 }}
     >
       <div className="ml-1 h3 pointer" style={{ display: "inline-block" }} onClick={hideTranscript} >
-        <FontAwesomeIcon icon={faCut} cursor="pointer" />
+        <FontAwesomeIcon icon={faCut} cursor="pointer" color="black" />
       </div>
       <div className="h5" style={{ display: "inline-block" }}>
-        click the above scissors if you wish to have a copy of this <i>opencert</i> file
+        Click the above scissors icon if you wish to have a copy of this <i>opencert</i> file
 		without your transcript. Then click on the &nbsp;
-		<FontAwesomeIcon icon={faEnvelope} cursor="pointer" />
-		&nbsp; button at the top right above, to email the file to the recipient or the &nbsp;
-		<FontAwesomeIcon icon={faFileDownload} cursor="pointer" />
+		<FontAwesomeIcon icon={faEnvelope} color="black" />
+		&nbsp; button, at the top right above, to email the file to the recipient or the &nbsp;
+		<FontAwesomeIcon icon={faFileDownload} color="black" />
 		&nbsp; button to download it to your computer.
       </div>
     </div>
