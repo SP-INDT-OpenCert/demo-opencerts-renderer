587f7381a653f1a5bdb69c97d452feb0ca92e379
SP-INDT-OpenCert <54525549+SP-INDT-OpenCert@users.noreply.github.com>
2020-02-25 16:12:08 +0800

    Create index.js
    header to be changed later

---
+++ src/components/templates/SPTemplate/PET/SP-2019-PFP/index.js
@@ -0,0 +1,17 @@
+import SPCertificate from './certificate';
+import SPTranscript from './transcript';
+
+const templates = [
+  {
+    id: 'certificate',
+    label: 'Certificate',
+    template: SPCertificate
+  },
+  {
+    id: 'transcript',
+    label: 'Transcript',
+    template: SPTranscript
+  }
+];
+
+export default templates;
