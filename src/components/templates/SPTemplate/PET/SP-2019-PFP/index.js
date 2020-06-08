c3bd39a6910390401684d6e3195d832d4fb7fd60
SP-INDT-OpenCert <54525549+SP-INDT-OpenCert@users.noreply.github.com>
2020-06-08 14:54:37 +0800

    for PFP
    for PFP

--- src/components/templates/SPTemplate/PET/SP-2019-PFP/index.js
+++ src/components/templates/SPTemplate/PET/SP-2019-PFP/index.js
@@ -1,16 +1,10 @@
 import SPCertificate from './certificate';
-import SPTranscript from './transcript';
 
 const templates = [
   {
     id: 'certificate',
     label: 'Certificate',
     template: SPCertificate
-  },
-  {
-    id: 'transcript',
-    label: 'Transcript',
-    template: SPTranscript
   }
 ];
 
--- src/components/templates/index.js
+++ src/components/templates/index.js
@@ -9,6 +9,7 @@ import CustomTemplate from "./customTemplate";
 import GovTechDemoCert from "./govtechDemoCert";
 import SP_2019_MAIN from "./SPTemplate/PET/SP-2019-MAIN";
 import SP_2019_DPCERT from "./SPTemplate/PET/SP-2019-DPCERT";
+import SP_2019_PFP from "./SPTemplate/PET/SP-2019-PFP";
 import SP_PACE_2019_DIP from "./SPTemplate/CET/SP_PACE_2019_DIP";
 import SP_PACE_2019_COC from "./SPTemplate/CET/SP_PACE_2019_COC";
 import SP_PACE_2019_PMC from "./SPTemplate/CET/SP_PACE_2019_PMC";
@@ -19,6 +20,7 @@ export default {
   GOVTECH_DEMO: GovTechDemoCert,
   SP_2019_MAIN: SP_2019_MAIN,
   SP_2019_DPCERT: SP_2019_DPCERT,
+  SP_2019_PFP: SP_2019_PFP,
   SP_PACE_2019_DIP: SP_PACE_2019_DIP,
   SP_PACE_2019_COC: SP_PACE_2019_COC,
   SP_PACE_2019_PMC: SP_PACE_2019_PMC,
