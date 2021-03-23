b7aca6aa96b53959dc796b4028cfdf5a6d115f61
SP-INDT-OpenCert <YU_Yat_Hong@sp.edu.sg>
2021-03-23 11:23:41 +0800

    Update certificate.js

--- src/components/templates/SPTemplate/PET/SP-2019-MAIN/certificate.js
+++ src/components/templates/SPTemplate/PET/SP-2019-MAIN/certificate.js
@@ -39,8 +39,8 @@ return (
   <div className="h5 mt-4 d-flex justify-content-center"
     style={{fontFamily:'Times'}}>
     It is hereby certified that</div>
-  <div className="h3 mt-4 mb-0 justify-content-center d-flex"
-	style={{fontFamily:'Monotype Corsiva,Garamond'}} >
+  <div className="h5 mt-4 mb-0 justify-content-center font-weight-bold font-italic d-flex"
+	style={{fontFamily:'Georgia italic'}} >
 	{document.recipient.name} </div>
 
   <div className="row my-0"><div className="col"></div>
@@ -57,8 +57,8 @@ return (
     <div className="col-1 h5 mt-4 d-flex justify-content-center"
 	style={{fontFamily:'Times'}} > on </div>
     <div className="col-3">
-	  <div className="h4 mt-3 mb-0 d-flex justify-content-center"
-		style={{fontFamily:'Monotype Corsiva,Garamond'}} >
+	  <div className="h5 mt-3 mb-0 d-flex justify-content-center font-italic"
+		style={{fontFamily:'Georgia italic'}} >
 		{format(document.graduationDate, 'D MMMM YYYY')} </div>
 	  <div className="row my-0">
         <div className="col" style={{borderBottomWidth:'1px', borderTopWidth:'0px', borderStyle:'solid', borderColor:'#333'}} ></div>
