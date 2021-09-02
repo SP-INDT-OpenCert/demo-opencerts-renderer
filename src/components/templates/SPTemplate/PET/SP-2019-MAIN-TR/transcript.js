c9613a9b268f2a7fac038821fab7691d58bd5978
SP-INDT-OpenCert <SP.INDT.Opencert@gmail.com>
2021-09-02 09:32:44 +0800

    Update transcript.js

--- src/components/templates/SPTemplate/PET/SP-2019-MAIN-TR/transcript.js
+++ src/components/templates/SPTemplate/PET/SP-2019-MAIN-TR/transcript.js
@@ -198,18 +198,17 @@ const renderTermTranscript = (p_ObjTranscript,iSn) => {
     ) : "";
   const objEFWcerts = get(p_ObjTranscript, 'EFWcerts');
   const dispEFWcerts = objEFWcerts ? objEFWcerts.map((objItm,i) => (
+     <div>
       <div className="row">
-        <div className="col-11" > <u>CERTIFICATE AWARDED</u> </div>
-        <div className="col" />
+        <div className="col-11" > <u>CERTIFICATE AWARDED</u> </div> <div className="col" />
       </div>
       <div className="row">
-        <div className="col-11" > {objItm.name } </div>
-        <div className="col" />
+        <div className="col-11" > {objItm.name } </div> <div className="col" />
       </div>
       <div className="row">
-        <div className="col-11" > {objItm.modList } </div>
-        <div className="col" />
+        <div className="col-11" > {objItm.modList } </div> <div className="col" />>
       </div>
+     </div>
 	  )
     ) : "";
   const objModules = get(p_ObjTranscript, 'modules');
