22777ccd23db17347dc180725c1da2d399d66235
SP-INDT-OpenCert <SP.INDT.Opencert@gmail.com>
2023-02-02 15:18:57 +0800

    Update index.tsx

--- src/templates/SPTemplate/PET/SP-2019-MAIN-TR/index.tsx
+++ src/templates/SPTemplate/PET/SP-2019-MAIN-TR/index.tsx
@@ -1,16 +1,10 @@
 import SPCertificate from "./certificate";
-import SPTranscript from "./transcript";
 
 const templates = [
   {
     id: 'certificate',
     label: 'CertificateTST',
     template: SPCertificate
-  },
-  {
-    id: 'transcript',
-    label: 'TranscriptTST',
-    template: SPTranscript
   }
 ];
 
