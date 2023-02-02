9b976d7f56ce284154aeed483bd8646c42af80b9
SP-INDT-OpenCert <SP.INDT.Opencert@gmail.com>
2023-02-02 18:49:03 +0800

    remove babel transform

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -12,7 +12,7 @@ import { templates as sp2019DipPlusTemplate } from "./SPTemplate/PET/SP-2019-DPC
 import { templates as sp2019PFPTemplate } from "./SPTemplate/PET/SP-2019-PFP";
 import { templates as sp2019mainTRTemplate } from "./SPTemplate/PET/SP-2019-MAIN-TR";
 import { templates as SP_PACE_2019_DIPtemplate } from "./SPTemplate/CET/SP_PACE_2019_DIP";
-// import { templates as SP_PACE_2019_COCtemplate } from "./SPTemplate/CET/SP_PACE_2019_COC";
+import { templates as SP_PACE_2019_COCtemplate } from "./SPTemplate/CET/SP_PACE_2019_COC";
 // import { templates as SP_PACE_2019_PMCtemplate } from "./SPTemplate/CET/SP_PACE_2019_PMC";
 
 import { TemplateRegistry } from "@govtechsg/decentralized-renderer-react-components";
@@ -27,7 +27,7 @@ export const registry: TemplateRegistry<any> = {
   SP_2019_DPCERT: sp2019DipPlusTemplate,
   SP_2019_PFP: sp2019PFPTemplate,
   SP_2019_MAIN_TR: sp2019mainTRTemplate,
-  SP_PACE_2019_DIP: SP_PACE_2019_DIPtemplate
-//  SP_PACE_2019_COC: SP_PACE_2019_COCtemplate,
+  SP_PACE_2019_DIP: SP_PACE_2019_DIPtemplate,
+  SP_PACE_2019_COC: SP_PACE_2019_COCtemplate
 //  SP_PACE_2019_PMC: SP_PACE_2019_PMCtemplate
 };
