d2593774cda3f456de294e5ad33319ed5c7a44ab
SP-INDT-OpenCert <SP.INDT.Opencert@gmail.com>
2023-02-02 14:04:28 +0800

    Update index.tsx

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,7 +1,7 @@
 import { templates as govtechDemoTemplates } from "./govtech-demo-cert";
 import { templates as testimonialsTemplates, templates2 as testimonialsTemplates2 } from "./testimonials";
 import { templates as certificateOfAwardTemplates } from "./certificate-of-award";
-import { templates as SP_2019_MAIN_TR } from "./SPTemplate/PET/SP-2019-MAIN-TR";
+import { templates as mainTR } from "./SPTemplate/PET/SP-2019-MAIN-TR";
 import { TemplateRegistry } from "@govtechsg/decentralized-renderer-react-components";
 
 export const registry: TemplateRegistry<any> = {
@@ -9,5 +9,6 @@ export const registry: TemplateRegistry<any> = {
   GOVTECH_DEMO: govtechDemoTemplates,
   TESTIMONIALS: testimonialsTemplates,
   TESTIMONIALS2: testimonialsTemplates2,
-  CERTIFICATE_OF_AWARD: certificateOfAwardTemplates
+  CERTIFICATE_OF_AWARD: certificateOfAwardTemplates,
+  SP_2019_MAIN_TR: mainTR
 };
