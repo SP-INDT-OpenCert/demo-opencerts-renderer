d1c79c69432fb0f083e5da3ea6dd616793bb4158
SP-INDT-OpenCert <SP.INDT.Opencert@gmail.com>
2023-02-06 12:37:02 +0800

    Update transcript.js

--- src/templates/SPTemplate/PET/SP-2019-MAIN-TR/transcript.js
+++ src/templates/SPTemplate/PET/SP-2019-MAIN-TR/transcript.js
@@ -202,7 +202,6 @@ const renderTermTranscript = (p_ObjTranscript,iSn) => {
 
   const dispEFWcerts = objEFWcerts ? objEFWcerts.map((objItm,i) => (
      <div>
-     // {dispEFWCaption(i)}
 
       <div className="row">
         <div className="col-11" > {objItm.name } </div> <div className="col" />
