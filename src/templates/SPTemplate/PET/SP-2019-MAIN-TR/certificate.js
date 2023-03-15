95995a264e27eeee36b80a127da9c08984a4b268
SP-INDT-OpenCert <SP.INDT.Opencert@gmail.com>
2023-03-15 18:07:52 +0800

    Update certificate.js

--- src/templates/SPTemplate/PET/SP-2019-MAIN-TR/certificate.js
+++ src/templates/SPTemplate/PET/SP-2019-MAIN-TR/certificate.js
@@ -14,7 +14,7 @@ const addOnDisp = get(document,'additionalData.diplomaAddOn').map((txtArr,i) => 
 const DipDescrDisp = p_FullDescr => {
   <div className="h5 mt-3 d-flex justify-content-center"
 	style={{fontFamily:'Times'}} >
-    {document.additionalData.diplomaFullDescr} </div>
+    {p_FullDescr} </div>
 }
 /*
 const DipDescrDisp = get(document,'additionalData.diplomaFullDescr').map((txtArr,i) => (
