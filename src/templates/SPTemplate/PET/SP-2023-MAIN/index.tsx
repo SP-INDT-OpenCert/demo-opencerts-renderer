f05417a99263ba152b5c9bf5f66e2ad3678ddf21
SP-INDT-OpenCert <SP.INDT.Opencert@gmail.com>
2023-03-20 14:37:03 +0800

    2023 template

---
+++ src/templates/SPTemplate/PET/SP-2023-MAIN/index.tsx
@@ -0,0 +1,17 @@
+import SPCertificate from './certificate';
+import SPTranscript from './transcript';
+
+export const templates = [
+  {
+    id: 'certificate',
+    label: 'Certificate',
+    template: SPCertificate
+  },
+  {
+    id: 'transcript',
+    label: 'Transcript',
+    template: SPTranscript
+  }
+];
+
+
--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -11,6 +11,7 @@ import { templates as sp2019mainTemplate } from "./SPTemplate/PET/SP-2019-MAIN";
 import { templates as sp2019DipPlusTemplate } from "./SPTemplate/PET/SP-2019-DPCERT";
 import { templates as sp2019PFPTemplate } from "./SPTemplate/PET/SP-2019-PFP";
 import { templates as sp2019mainTRTemplate } from "./SPTemplate/PET/SP-2019-MAIN-TR";
+import { templates as sp2023mainTemplate } from "./SPTemplate/PET/SP-2023-MAIN";
 import { templates as SP_PACE_2019_DIPtemplate } from "./SPTemplate/CET/SP_PACE_2019_DIP";
 import { templates as SP_PACE_2019_COCtemplate } from "./SPTemplate/CET/SP_PACE_2019_COC";
 import { templates as SP_PACE_2019_PMCtemplate } from "./SPTemplate/CET/SP_PACE_2019_PMC";
@@ -24,6 +25,7 @@ export const registry: TemplateRegistry<any> = {
   TESTIMONIALS2: testimonialsTemplates2,
   CERTIFICATE_OF_AWARD: certificateOfAwardTemplates,
   SP_2019_MAIN: sp2019mainTemplate,
+  SP_2023_MAIN: sp2023mainTemplate,
   SP_2019_DPCERT: sp2019DipPlusTemplate,
   SP_2019_PFP: sp2019PFPTemplate,
   SP_2019_MAIN_TR: sp2019mainTRTemplate,
