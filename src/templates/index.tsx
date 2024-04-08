38ad7e20f22fc25cbb1ad54cb06077ea69b5a2a8
SP-INDT-OpenCert <54525549+SP-INDT-OpenCert@users.noreply.github.com>
2024-04-08 15:39:02 +0800

    spRyanTestTemplate
    spRyanTestTemplate

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -16,7 +16,7 @@ import { templates as sp2024mainTemplate } from "./SPTemplate/PET/SP-2024-MAIN";
 import { templates as sp2024PFPTemplate } from "./SPTemplate/PET/SP-2024-PFP";
 import { templates as sp2024DipPlusTemplate } from "./SPTemplate/PET/SP-2024-DPCERT";
 import { templates as sp2023DipPlusTemplate } from "./SPTemplate/PET/SP-2023-DPCERT";
-import { templates as SP_RYAN_TEST } from "./SPTemplate/PET/SP-RYAN-TEST";
+import { templates as spRyanTestTemplate } from "./SPTemplate/PET/SP-RYAN-TEST";
 import { templates as SP_PACE_2019_DIPtemplate } from "./SPTemplate/CET/SP_PACE_2019_DIP";
 import { templates as SP_PACE_2019_COCtemplate } from "./SPTemplate/CET/SP_PACE_2019_COC";
 import { templates as SP_PACE_2019_PMCtemplate } from "./SPTemplate/CET/SP_PACE_2019_PMC";
@@ -40,7 +40,7 @@ export const registry: TemplateRegistry<any> = {
   SP_2024_MAIN: sp2024mainTemplate,
   SP_2024_PFP: sp2024PFPTemplate,
   SP_2024_DPCERT: sp2024DipPlusTemplate,
-  SP_RYAN_TEST: SP_RYAN_TEST,
+  SP_RYAN_TEST: spRyanTestTemplate,
   SP_PACE_2019_DIP: SP_PACE_2019_DIPtemplate,
   SP_PACE_2019_COC: SP_PACE_2019_COCtemplate,
   SP_PACE_2019_PMC: SP_PACE_2019_PMCtemplate,
