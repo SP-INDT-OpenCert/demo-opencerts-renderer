fb4454010f2ee91f9504669a62d4b785c64e3036
SP-INDT-OpenCert <54525549+SP-INDT-OpenCert@users.noreply.github.com>
2024-04-09 15:18:42 +0800

    Test

--- src/templates/SPTemplate/PET/SP-2025-EM/certificate.tsx
+++ src/templates/SPTemplate/PET/SP-2025-EM/certificate.tsx
@@ -3,7 +3,7 @@ import { get } from "lodash";
 import styled from "@emotion/styled";
 import React, { FunctionComponent } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import "./common/demo-styles.css";
+//import "./common/demo-styles.css";
 import { TemplateProps } from "@govtechsg/decentralized-renderer-react-components";
 import { GovtechOpencertsTemplateCertificate } from "../samples";
 //import { PrintWatermark } from "./common/print-watermark";
@@ -168,76 +168,13 @@ export const CertificateTemplate: FunctionComponent<TemplateProps<GovtechOpencer
 }) => (
   <Page certificateBg={`url('${certificateBg}')`} className="p-4">
     {/* <PrintWatermark /> */}
-    <section className="text-center">
-      <img className="img-fluid logo" src={IMG_CERT_SPLOGO} alt="SP Logo" />
-      
-      <br />
 
-      <div className="spacer text-lg">
-        <b>{document.name}</b>
-      </div>
-      <div className="spacer text-sm">
-        It is hereby certified that
-      </div>
-       <div className="spacer text-md-nm">
-        <i>{document.recipient.name}</i>
-      </div>
-      
-      <namehr />
-      {/* <div className="row my-0"><div className="col"></div>
-          <div className="col-9" style={{borderBottomWidth:'1px', borderTopWidth:'0px', borderStyle:'solid', borderColor:'#333'}} ></div>
-      <div className="col"></div></div> */}
-      
-      <div className="spacer text-sm">
-        has successfully completed the <i>{document.name}</i>
-      </div>
-
-      <div className="row my-0">
+    <div className="container" style={{width:'800px', backgroundColor:'rgb(255,255,238)', borderStyle:'solid', borderColor:'rgb(204,204,170)'}}>
+      <div className="row">
         <div className="col"></div>
-        <div className="col-1 h5 mt-4 d-flex justify-content-center" style={{fontFamily:'Times'}} > on </div>
-        <div className="col-3">
-	        <div className="h5 mt-3 mb-0 d-flex justify-content-center font-italic" style={{fontFamily:'Georgia italic'}}> {format(document.graduationDate, 'D MMMM YYYY')} </div>
-	        <div className="row my-0">
-            <div className="col" style={{borderBottomWidth:'1px', borderTopWidth:'0px', borderStyle:'solid', borderColor:'#333'}} ></div>
-          </div>
-	      </div>
+        <div className="col my-5 justify-content-center"><img src={IMG_CERT_SPLOGO} alt="SP Logo" width='396' height='128'/></div>
         <div className="col"></div>
       </div>
-     
-      {/*<div className="spacer text-sm">on</div>
-      <div className="spacer text-md">
-      {format(document.graduationDate, 'D MMMM YYYY')}
-      </div> */}
-
-      <br />
-
-      <div className="spacer">
-        <img src={IMG_CERT_SEAL} className="img-fluid seal" alt="SP Seal" />
-      </div>
-      
-    </section>
-    {/* <section>
-      <div className="row align-items-center">
-        <div className="col">
-          <div className="text-center text-sm">
-            <img
-              className="img-fluid signature"
-              src={get(document, "additionalData.certSignatories[0].signature")}
-              alt="Signature"
-            />
-            <hr style={{ backgroundColor: "#333" }} />
-            <div>
-              <b>{get(document, "additionalData.certSignatories[0].name")}</b>
-              <br />
-              {get(document, "additionalData.certSignatories[0].position")}
-            </div>
-          </div>
-        </div>
-        <div className="col" />
-        <div className="col">
-          <div className="text-sm text-right">{document.recipient.studentId}/{document.recipient.nric}</div>
-        </div>
-      </div>
-    </section> */}
+    </div>
   </Page>
 );
