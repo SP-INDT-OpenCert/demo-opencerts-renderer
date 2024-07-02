0b47a0cc6e42d909a397e881fbd97a48f0072580
SP-INDT-OpenCert <SP.INDT.Opencert@gmail.com>
2024-07-02 12:17:23 +0800

    Diploma Name Alignment Issue 02/07/2024
    Diploma Name Alignment Issue 02/07/2024

--- src/templates/SPTemplate/PET/SP-2024-MAIN/certificate.js
+++ src/templates/SPTemplate/PET/SP-2024-MAIN/certificate.js
@@ -6,7 +6,7 @@ import { IMG_CERT_SPLOGO, IMG_CERT_SEAL } from './Certimages'
 
 const Template = ({ document }) => {
 const addOnDisp = get(document,'additionalData.diplomaAddOn').map((txtArr,i) => (
-  <div className="h2 my-0 font-weight-bold d-flex justify-content-center"
+  <div className="h2 my-0 font-weight-bold d-flex justify-content-center text-center"
     style={{fontFamily:'Times', fontVariant:'small-caps'}} >
     {txtArr} </div>)
 )
