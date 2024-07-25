75707ac0e0af9f80995cd98f5c5c7c691b2ed3a2
SP-INDT-OpenCert <SP.INDT.Opencert@gmail.com>
2024-07-25 11:42:06 +0800

    Module Exclusion
    Module Exclusion

--- src/templates/SPTemplate/PET/SP-2024-MAIN-CLONE/transcript.js
+++ src/templates/SPTemplate/PET/SP-2024-MAIN-CLONE/transcript.js
@@ -51,7 +51,7 @@ const renderFooter = p_Cert => (
   {/* SR-2023-8348 Start S10131508 17/07/2024 
    <div className="col-8" /> */}
    {/* <div className="col-8"> */}
-	   <div className="col-8"
+	   <div className="col"
 	style={{fontFamily:'Times', fontSize:'0.68rem'}} > {document.additionalData.transcriptSignatories[0].moduleexclude} </div> 
 		{/* </div> */}
 	{/* SR-2023-8348 End S10131508 17/07/2024 */}
