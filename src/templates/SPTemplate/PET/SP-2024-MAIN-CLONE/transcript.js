1de17858d43ed41483b9f4d2f907164467db8acc
SP-INDT-OpenCert <SP.INDT.Opencert@gmail.com>
2024-07-25 17:03:25 +0800

    Update transcript.js

--- src/templates/SPTemplate/PET/SP-2024-MAIN-CLONE/transcript.js
+++ src/templates/SPTemplate/PET/SP-2024-MAIN-CLONE/transcript.js
@@ -56,18 +56,21 @@ const renderFooter = p_Cert => (
    {/* SR-2023-8348 End S10131508 17/07/2024 */}
   <div className="row">
   {/* SR-2023-8348 Start S10131508 17/07/2024 */} 
-	  {/*	<div className="col-8" />
+  <div className="col-2" />
+    <div className="col-3">
+	    <div className="text-center"> </div>
+        <div className="text-center"> </div>
+        <div className="text-center" style={{fontFamily:'Times', fontSize:'0.68rem'}} > {document.additionalData.transcriptSignatories[0].moduleexclude} </div>
+    </div>
+   {/* SR-2023-8348 End S10131508 17/07/2024 */}
+ 	<div className="col-8" />
     <div className="col-3">
 	    <div className="text-center"><img src={get(p_Cert, 'additionalData.transcriptSignatories[0].signature')} alt="sign" style={{width:'160px'}} /></div>
         <div style={{borderBottomWidth:'1px', borderTopWidth:'0px', borderStyle:'solid', borderColor:'#333'}} ></div>
         <div className="text-center font-weight-bold"
 		style={{fontFamily:'Times', fontSize:'12px'}} > {document.additionalData.transcriptSignatories[0].position} </div>
     </div>
-	  <div className="col-1" /> */}
-	<div className="col-2" style={{fontFamily:'Times', fontSize:'0.68rem'}} > {document.additionalData.transcriptSignatories[0].moduleexclude} </div>
-	<div className="text-center font-weight-bold"
-		style={{fontFamily:'Times', fontSize:'12px'}} > {document.additionalData.transcriptSignatories[0].position} </div>
-  {/* SR-2023-8348 Start S10131508 17/07/2024 */} 
+	  <div className="col-1" /> 
   </div>
   <div className="row" style={{height:'48px'}} ></div>
     
