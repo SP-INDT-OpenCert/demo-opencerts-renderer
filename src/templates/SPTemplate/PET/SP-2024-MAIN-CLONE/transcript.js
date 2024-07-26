9fa2d2a7f1019cb4db551e7791f591674363d47d
SP-INDT-OpenCert <SP.INDT.Opencert@gmail.com>
2024-07-26 12:27:23 +0800

    Update transcript.js

--- src/templates/SPTemplate/PET/SP-2024-MAIN-CLONE/transcript.js
+++ src/templates/SPTemplate/PET/SP-2024-MAIN-CLONE/transcript.js
@@ -89,13 +89,16 @@ const renderFooter = p_Cert => (
 	{/* SR-2023-8348 End S10131508 17/07/2024 */} 
   </div>
   {/* SR-2023-8348 Start S10131508 17/07/2024 */} 
-	  {/*
   <div className="row">
     <div className="col-1" />
 	<div className="col-7">
+	   <div className="text-left" style={{fontFamily:'Times', fontSize:'0.68rem'}} > &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{document.additionalData.transcriptSignatories[0].moduleexclude} </div>
+	</div>
+	<div className="col-3">
+	   <div className="text-center font-weight-bold"
+	   style={{fontFamily:'Times', fontSize:'12px'}} > {document.additionalData.transcriptSignatories[0].position} </div>
 	</div>
   </div>
-	  */}
   {/* SR-2023-8348 End S10131508 17/07/2024 */} 
   <div className="row" style={{height:'48px'}} ></div>
     
