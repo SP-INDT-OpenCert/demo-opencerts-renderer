06f0780cf8155dff065946d123dae2c4fc137ff1
SP-INDT-OpenCert <SP.INDT.Opencert@gmail.com>
2024-07-26 15:28:58 +0800

    Update transcript.js

--- src/templates/SPTemplate/PET/SP-2024-MAIN-CLONE/transcript.js
+++ src/templates/SPTemplate/PET/SP-2024-MAIN-CLONE/transcript.js
@@ -67,14 +67,14 @@ const renderFooter = p_Cert => (
 	  <div className="col-1" /> SR-2023-8348 S10131508 25/07/2024 
 	   */}
 	   <div className="col-1" />
-	<div className="col-7">
+	<div className="col-8">
 	  <div className="row" />
 	  <div className="row" />
       {/*<div className="row">
 	  <div className="text-left" style={{fontFamily:'Times', fontSize:'0.68rem'}} > &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{document.additionalData.transcriptSignatories[0].moduleexclude} </div>
       </div>*/}
 	</div>
-    <div className="col-4">
+    <div className="col-3">
 	  <div className="row">
 	    <div className="text-left"><img src={get(p_Cert, 'additionalData.transcriptSignatories[0].signature')} alt="sign" style={{width:'160px'}} /></div>
 	  </div>
@@ -91,10 +91,10 @@ const renderFooter = p_Cert => (
   {/* SR-2023-8348 Start S10131508 17/07/2024 */} 
   <div className="row">
     <div className="col-1" />
-	<div className="col-7">
+	<div className="col-8">
 	   <div className="text-left" style={{fontFamily:'Times', fontSize:'0.68rem'}} >&nbsp;&nbsp;&nbsp;{document.additionalData.transcriptSignatories[0].moduleexclude} </div>
 	</div>
-	<div className="col-4">
+	<div className="col-3">
 	   <div className="text-left font-weight-bold"
 	   style={{fontFamily:'Times', fontSize:'12px'}} > {document.additionalData.transcriptSignatories[0].position} </div>
 	</div>
