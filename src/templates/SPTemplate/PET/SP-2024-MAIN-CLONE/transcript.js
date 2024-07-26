d37749a2afde5e2293df01646a263257b89b6d70
SP-INDT-OpenCert <SP.INDT.Opencert@gmail.com>
2024-07-26 15:43:15 +0800

    Update transcript.js

--- src/templates/SPTemplate/PET/SP-2024-MAIN-CLONE/transcript.js
+++ src/templates/SPTemplate/PET/SP-2024-MAIN-CLONE/transcript.js
@@ -68,19 +68,21 @@ const renderFooter = p_Cert => (
 	   */}
 	   <div className="col-1" />
 	<div className="col-7">
-	  <div className="row" />
-	  <div className="row" />
+	{/*<div className="row" />
+	<div className="row" /> */}
       {/*<div className="row">
 	  <div className="text-left" style={{fontFamily:'Times', fontSize:'0.68rem'}} > &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{document.additionalData.transcriptSignatories[0].moduleexclude} </div>
       </div>*/}
 	</div>
     <div className="col-3">
-	  <div className="row">
+	{/*<div className="row">
 	    <div className="text-center"><img src={get(p_Cert, 'additionalData.transcriptSignatories[0].signature')} alt="sign" style={{width:'160px'}} /></div>
 	  </div>
 	  <div className="row">
 	     <div className="text-center" style={{borderBottomWidth:'1px', borderTopWidth:'0px', borderStyle:'solid', borderColor:'#333'}} ></div>
-	  </div>
+	</div>*/}
+	   <div className="text-center"><img src={get(p_Cert, 'additionalData.transcriptSignatories[0].signature')} alt="sign" style={{width:'160px'}} /></div>
+	   <div className="text-center" style={{borderBottomWidth:'1px', borderTopWidth:'0px', borderStyle:'solid', borderColor:'#333'}} ></div>
       {/*<div className="row">
 	  <div className="text-center font-weight-bold"
 	  style={{fontFamily:'Times', fontSize:'12px'}} > {document.additionalData.transcriptSignatories[0].position} </div>
