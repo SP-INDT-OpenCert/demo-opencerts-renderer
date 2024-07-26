b39b5a0809393d4c7b7bcf5a74b89810eeae4813
SP-INDT-OpenCert <SP.INDT.Opencert@gmail.com>
2024-07-26 14:18:20 +0800

    Update transcript.js

--- src/templates/SPTemplate/PET/SP-2024-MAIN-CLONE/transcript.js
+++ src/templates/SPTemplate/PET/SP-2024-MAIN-CLONE/transcript.js
@@ -79,7 +79,7 @@ const renderFooter = p_Cert => (
 	    <div className="text-center"><img src={get(p_Cert, 'additionalData.transcriptSignatories[0].signature')} alt="sign" style={{width:'160px'}} /></div>
 	  </div>
 	  <div className="row">
-  (/*<div style={{borderBottomWidth:'1px', borderTopWidth:'0px', borderStyle:'solid', borderColor:'#333'}} >________________</div>*/}
+	  {/*<div style={{borderBottomWidth:'1px', borderTopWidth:'0px', borderStyle:'solid', borderColor:'#333'}} >________________</div>*/}
 	     <div className="text-center">__________________</div>
 	  </div>
       <div className="row">
