28764d09ce5a9dc9e108e36238ec2dea0eb7d91c
SP-INDT-OpenCert <SP.INDT.Opencert@gmail.com>
2024-07-26 12:30:38 +0800

    Update transcript.js

--- src/templates/SPTemplate/PET/SP-2024-MAIN-CLONE/transcript.js
+++ src/templates/SPTemplate/PET/SP-2024-MAIN-CLONE/transcript.js
@@ -92,9 +92,9 @@ const renderFooter = p_Cert => (
   <div className="row">
     <div className="col-1" />
 	<div className="col-7">
-	   <div className="text-left" style={{fontFamily:'Times', fontSize:'0.68rem'}} > &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{document.additionalData.transcriptSignatories[0].moduleexclude} </div>
+	   <div className="text-left" style={{fontFamily:'Times', fontSize:'0.68rem'}} >&nbsp;&nbsp;&nbsp;{document.additionalData.transcriptSignatories[0].moduleexclude} </div>
 	</div>
-	<div className="col-3">
+	<div className="col-2">
 	   <div className="text-center font-weight-bold"
 	   style={{fontFamily:'Times', fontSize:'12px'}} > {document.additionalData.transcriptSignatories[0].position} </div>
 	</div>
